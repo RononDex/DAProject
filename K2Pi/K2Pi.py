5dff2839382eaaa39115be449302c30af5313eb3
SpaceCatKnight <SpaceCatKnight@users.noreply.github.com>
2016-12-02 10:48:00 +0100

    Update K2Pi.py

--- K2Pi/K2Pi.py
+++ K2Pi/K2Pi.py
@@ -91,3 +91,50 @@ d_0 =(a-z_length) * np.tan(GetAngleBetweenVectors(p_0,ez))
 d_plus =(a-z_length) * np.tan(GetAngleBetweenVectors(p_plus,ez))
 print(d_0)
 print(d_plus)
+
+'''Das Skript in sauberer Form:'''
+
+#functions:
+
+def GetAngleBetweenVectors(X, Y):
+    x=np.linalg.norm(X)
+    y=np.linalg.norm(Y)
+    return np.arccos(np.dot(X,Y)/(x*y))
+
+def LorentzBoost(b,g):
+    return np.array([[g,0,0,b*g],[0,1,0,0],[0,0,1,0],[b*g,0,0,g]])
+
+def SimulateDecay(E_K_0, E_K_plus, p, b, g, a, tau):
+    theta = np.array(stats.uniform.rvs(scale=np.pi, size=1))
+    phi = np.array(stats.uniform.rvs(scale=2*np.pi, size=1))
+    x0= np.sin(theta)*np.cos(phi)
+    y0= np.sin(theta)*np.sin(phi)
+    z0= np.cos(theta)
+    
+    P_K_0 = np.array([E_K0,p*x0,p*y0,p*z0])
+    P_K_plus = np.array([E_K_plus,p*(-x0),p*(-y0),p*(-z0)])
+
+    P_lab_0 = np.dot(LorentzBoost(b,g),P_K_0.T)
+    P_lab_plus = np.dot(LorentzBoost(b,g),P_K_plus.T)
+    
+    z_length= np.array(stats.expon.rvs(loc=0, scale=tau, size=1))
+    ez= np.array([0,0,1])
+
+    d_0 = float((a-z_length) * np.tan(GetAngleBetweenVectors(P_lab_0[1:],ez)))
+    d_plus = float((a-z_length) * np.tan(GetAngleBetweenVectors(P_lab_plus[1:],ez)))
+    
+    return d_0, d_plus
+
+#parameters:
+
+E_K_0 = 245.563588 #MeV         #Energie der neutrale Pionen in K+ system
+E_K_plus = 248.118174 #MeV      #Energie der positiven Pionen in K+ system
+p = 205.14091 #MeV/c            #Impulsbetrag der Pionen (der selbe für beide)
+b = 0.99997833784995            #Betafaktor
+g = 151.92756392754             #Gammafaktor
+a = 280                         #Totale Distanz zum Detektor
+tau = 132.97                    #Mittlere Zerfallsstrecke von K+
+
+#Auswertung:
+
+print(SimulateDecay(E_K_0, E_K_plus, p, b, g, a, tau))
