eb384225969b6827daa6ffa66391ba60f06e2c24
norasalgo <nora.salgo@uzh.ch>
2016-12-08 23:13:27 +0100

    Update TauEstimatorNora.py

--- K2Pi/TauEstimatorNora.py
+++ K2Pi/TauEstimatorNora.py
@@ -1,14 +1,21 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Dec  7 18:50:27 2016
+Created on Thu Dec  8 14:24:15 2016
 
-@author: Nora
+@author: nsalgo
 """
 
 import numpy as np
 import scipy.stats as stats
+from threading import Thread
+import matplotlib.pyplot as plt
+import multiprocessing
+from multiprocessing.pool import ThreadPool
+import threading
 
-data = np.loadtxt('dec_lengths.dat')
+
+
+data = np.loadtxt('dec_lengths.dat')        #100'000 measurements of decay length of kaons and pions
 
 n=len(data)
 tau_pi=4188                                 #average decay length of pions, given
@@ -16,21 +23,62 @@ frac_pi=0.84                                #fraction of the pions in the beam
 
 bns = 100    
 n_tot, b = np.histogram(data, bins=bns)     #histogram of given data
-x=b+(max(b)-min(b))/(2*bns)                     #center of bins
+x=b+(max(b)-min(b))/(2*bns)                 #center of bins
 x=x[:-1] 
 
-N=100
-tau_k_estimator=[]
-for i in range(N):
-    dist_pi = stats.expon.rvs(scale=tau_pi, size=int(frac_pi*n))    #exponential distribution with tau=average decay length of pions
-    n_pi, b = np.histogram(dist_pi, bins=int(bns), range=[min(data),max(data)])
 
-    n_k=n_tot-n_pi                          #values of histogram of decay length of kaons
-    tau_k_est = sum(x*n_k)/sum(n_k)         #average decay length of kaons
-    tau_k_estimator.append(tau_k_est)
-
-tau_k_estimator=np.array(tau_k_estimator)   
-tau_k_estimator=np.mean(tau_k_estimator)    #mean of average decay length pf kaons
+def startmultithreading(enableMultiThreading,N): 
+    if (enableMultiThreading):
+        number_of_cores = multiprocessing.cpu_count()
+        number_of_threads = number_of_cores
+        print("Running on %s cores, using %s threads" % (number_of_cores, number_of_threads))
 
 
-print(tau_k_estimator)
+    threads = []    
+    liste=[]
+
+    # Use Multithreading to start as many threads as we have cores on this machine
+    if (enableMultiThreading):
+        
+        nThread = int(N / number_of_threads) # The amount of simulation runs every thread should do is n / number_of_threads
+
+        for i in range(number_of_cores):
+            # Create mew thread
+            t = Thread(target=runwiththreads, args = (nThread,liste))
+            t.start();
+            threads.append(t);
+        for i in range(len(threads)):
+            if (threads[i].isAlive()):
+                threads[i].join()
+        return liste
+
+
+
+def runwiththreads(nThread,tau_k_estimator):
+    
+    for i in range(nThread):
+        dist_pi = stats.expon.rvs(scale=tau_pi, size=int(frac_pi*n))    #exponential distribution with scale==average decay length of pions
+        n_pi, b = np.histogram(dist_pi, bins=int(bns), range=[min(data),max(data)])
+
+        n_k=n_tot-n_pi                          #values of histogram of decay length of kaons
+        tau_k_est = sum(x*n_k)/sum(n_k)         #average decay length of kaons
+        tau_k_estimator.append(tau_k_est)       #list of average decay length of kaons
+
+    
+    
+N=1000                                          #How many times to calculate average decay length
+tau_estimator_liste=startmultithreading(True,N) #List of N average decay length estimations
+tau_estimator_array=np.array(tau_estimator_liste)
+
+average_decay_length=np.mean(tau_estimator_array) 
+std_k=np.std(tau_estimator_array)               #Standart deviation of distriribution of the tau estimators
+uncertainty_tau=std_k/np.sqrt(N)                #Uncertainity on mean of tau estimators
+
+print(average_decay_length)
+print(uncertainty_tau)
+
+plt.figure()                                    #Histogram of distribution of tau estimators
+plt.hist(tau_estimator_array, 15)
+plt.xlabel('Average decay length of the kaon [m]')
+#plt.savefig('hist_tau_estimator3.jpg', dpi=480)
+plt.show()
